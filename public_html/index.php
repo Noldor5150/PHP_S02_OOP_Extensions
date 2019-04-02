4e88d9270afe0e81a22c5cb30b9e6c7c4aae85cf
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-02 13:38:05 +0300

    kuriam boisu klases

--- public_html/index.php
+++ public_html/index.php
@@ -1,46 +1,70 @@
 <?php
 
-class Girl{
-    
+class Girl {
+
     protected $age;
-    
-        public function __construct($age) {
+
+    public function __construct($age) {
         $this->age = $age;
     }
 
-        public function beSmart(){
+    public function beSmart() {
         return 'smart';
     }
-    
-    public function beBeautiful(){
+
+    public function beBeautiful() {
         return 'beautiful';
     }
+
 }
 
-class Girlfriend extends Girl{
-    
-    public function pistiProta(){
+class Girlfriend extends Girl {
+
+    public function pistiProta() {
         return 'alio jebat';
     }
+
 }
 
-class Wife extends Girlfriend{
-        
-        public function  pistiProta(){
-            return 'gal rytoj?';
-        }
-    } 
+class Wife extends Girlfriend {
+
+    public function pistiProta() {
+        return 'gal rytoj?';
+    }
+
+}
 
 class HorribleWife extends Girlfriend {
-    
-    public function pistiProta(){
+
+    public function pistiProta() {
         return parent::pistiProta() . ' noretum is kiemo puses?';
     }
-    
-}
-    
-$boba = new HorribleWife(20);
-$message = $boba->pistiProta();
-print $message;
 
-var_dump($boba);
+}
+
+class Boy {
+
+    public function winArgument() {
+        return 'sian tavo diena...';
+    }
+
+}
+
+class Boyfriend extends Boy {
+
+    public function winArgument() {
+        $array = ['Not this time!', parent::winArgument()];
+        return $array[rand(0, 1)];
+    }
+
+}
+
+$boba = new HorribleWife(20);
+$message_bobos = $boba->pistiProta();
+print $message_bobos;
+
+$bicas = new Boyfriend;
+$message_bico  = $bicas->winArgument();
+print $message_bico;
+
+
