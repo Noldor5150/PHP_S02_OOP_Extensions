aad9640392ab2e6a6f4d10adf8214593fba4bf3b
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-02 13:05:00 +0300

    kuriam konstruktoriu girlklaseje, nustatinti age

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,13 @@
 
 class Girl{
     
-    public function beSmart(){
+    protected $age;
+    
+        public function __construct($age) {
+        $this->age = $age;
+    }
+
+        public function beSmart(){
         return 'smart';
     }
     
@@ -28,8 +34,13 @@ class Wife extends Girlfriend{
 class HorribleWife extends Girlfriend {
     
     public function pistiProta(){
-        return parent::pistiProta() . 'noretum is kiemo puses?';
+        return parent::pistiProta() . ' noretum is kiemo puses?';
     }
     
 }
     
+$boba = new HorribleWife(20);
+$message = $boba->pistiProta();
+print $message;
+
+var_dump($boba);
